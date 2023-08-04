6b64a8c4014fabe11574ceadd39b5c0fe3fa2ed1
Joonas Kuittinen <jokuitti@student.uef.fi>
2023-08-04 05:06:51 +0300

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,28 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response, next) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response, next) => {
   const { body } = request;
 
+  const users = await User.find({});
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
+    user: users[0]._id,
     url: body.url,
     likes: body.likes || 0,
   });
 
   const savedBlog = await blog.save();
+  users[0].blogs = users[0].blogs.concat(savedBlog._id)
+  await users[0].save()
   response.status(201).json(savedBlog);
 });
 
--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,7 @@ usersRouter.post('/', async (request, response) => {
 
 usersRouter.get('/', async (request, response) => {
   const users = await User
-    .find({}).populate('blogs');
+    .find({}).populate('blogs', { author: 1, title: 1, url: 1 });
 
   response.json(users);
 });
